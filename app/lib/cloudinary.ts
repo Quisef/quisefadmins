eeacf971adbe430d0cb94c0a3206ee541b295c76
tsoji78 <tsojivictor@gmail.com>
2025-01-29 18:12:38 +0100

    next config

--- app/lib/cloudinary.ts
+++ app/lib/cloudinary.ts
@@ -1,11 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary';
 
+// Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-  preset: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
-  secure: true,
+  secure: true, // Ensures HTTPS is used
 });
 
 export default cloudinary;
