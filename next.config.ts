9bf2c30b0622b65c93e212bbc7f8c3c6f6cf35cd
tsoji78 <tsojivictor@gmail.com>
2025-02-07 17:47:24 +0100

    next config

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // Enables static export
+  output: "export", // Enables static export
   images: {
     unoptimized: true, // Disables Next.js image optimization (required for static export)
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during build
   },
 };
 
