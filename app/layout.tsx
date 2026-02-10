ac559b6e3ed29b4dcd5105733e65e72b6b462417
tsoji78 <tsojivictor@gmail.com>
2026-02-09 19:11:21 -1100

    LOGO

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   description: 'Content Management System',
   icons: {
     icon: {
-      url: '/images/logo2.jpg', // Path relative to public
+      url: '/images/QSEF-10.PNG', // Path relative to public
       sizes: '128x128',         // Specify size (e.g., 32x32 pixels)
       type: 'image/jpg',      // MIME type
     },
